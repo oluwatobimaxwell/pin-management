ef789cedc512c0db13c423212fb797a37d4d3ffe
Omoyele Samuel <samuelomoyele777@gmail.com>
2022-10-08 15:53:07 +0100

    final push?

--- pin/views.py
+++ pin/views.py
@@ -4,16 +4,16 @@ from .models import Pin
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse()
     
 
-def get_amount():
-    field_name = 'amount'
-    obj = Pin.objects.first()
-    field_object = Pin._meta.get_field(field_name)
-    field_value = field_object.value_from_object(obj)
-    print(field_value)
+# def get_amount():
+#     field_name = 'amount'
+#     obj = Pin.objects.first()
+#     field_object = Pin._meta.get_field(field_name)
+#     field_value = field_object.value_from_object(obj)
+#     print(field_value)
 
-get_amount()
+# get_amount()
 
 

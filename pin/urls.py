95067bc59a812cc8f3cda7e9fb359f76720b1117
Omoyele Samuel <samuelomoyele777@gmail.com>
2022-11-21 15:10:49 +0100

    Added Logo to printed pins

--- pin/urls.py
+++ pin/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('generate/', views.generate_pin_form, name='generate'),
     path('generate-create/', views.generate_pin, name='create-pins'),
     path('print-pins/', views.print_pins, name='print-pins'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
